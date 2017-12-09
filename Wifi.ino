e8c51d9903d4804afd0b08d45e80797338b91895
lubosmarecek <34372550+lubosmarecek@users.noreply.github.com>
2017-12-09 07:42:45 +0100

    Add files via upload

---
+++ Wifi.ino
@@ -0,0 +1,92 @@
+#include <ESP8266WiFi.h>
+
+const char* ssid     = "hackathon";
+const char* password = "att4hack";
+
+const char* host = "ip-api.com";
+
+void setup() {
+  Serial.begin(115200);
+  delay(10);
+
+  // We start by connecting to a WiFi network
+
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  
+  WiFi.begin(ssid, password);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");  
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+int value = 0;
+
+String getValue(String data, char separator, int index){
+  int found = 0;
+  int strIndex[] = {0, -1};
+  int maxIndex = data.length()-1;
+
+  for(int i=0; i<=maxIndex && found<=index; i++){
+    if(data.charAt(i)==separator || i==maxIndex){
+        found++;
+        strIndex[0] = strIndex[1]+1;
+        strIndex[1] = (i == maxIndex) ? i+1 : i;
+    }
+  }
+
+  return found>index ? data.substring(strIndex[0], strIndex[1]) : "";
+}
+
+void loop() {
+  delay(5000);
+  ++value;
+
+  Serial.print("connecting to ");
+  Serial.println(host);
+  
+  // Use WiFiClient class to create TCP connections
+  WiFiClient client;
+  const int httpPort = 80;
+  if (!client.connect(host, httpPort)) {
+    Serial.println("connection failed");
+    return;
+  }
+
+  // We now create a URI for the request
+  //String url = "/jistise/serverTestArd.php?test=bla";
+  String url = "/json/?callback=yf";
+  
+  Serial.print("Requesting URL: ");
+  Serial.println(url);
+
+  // This will send the request to the server
+  client.print("GET " + url + " HTTP/1.1\r\n" +
+                   "Host: " + host + "\r\n" +
+                   "Content-Type: application/json\r\n" + 
+                   "Connection: close\r\n\r\n");
+      delay(500);
+      
+  String line;
+  // Read all the lines of the reply from server and print them to Serial
+  while(client.available()){
+    line = client.readStringUntil('\r');
+    //Serial.print(line);
+  }
+  String h = getValue(line, '"', 11);
+  String m = getValue(line, '"', 39);
+  String sub = getValue(line, '"', 7);
+  Serial.println(h);
+  Serial.println(m);
+  Serial.println(sub);
+  Serial.println("closing connection");
+}
